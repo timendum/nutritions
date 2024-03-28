e8d081e8abaea83ddf851639eb10c6d5362446c9
timendum <timedum@gmail.com>
2024-03-28 09:55:49 +0100

    Fix Coop debugging

--- debug_coop.py
+++ debug_coop.py
@@ -3,16 +3,19 @@ import json
 
 from coop import Coop
 
-slug = "kefir-bianco-naturale-500-ml-podere-cittadella"
+slug = "composta-di-albicocche-270-gr-alce-nero"
 
 coop = Coop("w0d0t0")
 prod = None
 for jfile in glob.glob("data/*.json"):
     with open(jfile) as fi:
         products = json.load(fi)
-    for p in products:
-        if slug == p["slug"]:
-            prod = p
-            break
+    try:
+        for p in products:
+            if slug == p.get("slug", ""):
+                prod = p
+                break
+    except:
+        continue
 if prod:
     coop._save_products([prod])
